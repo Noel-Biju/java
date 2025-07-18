81f2caa7059fb02bc96550373779fa8bd44c7a17
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2025-07-18 10:28:35 +0530

    Create parameterconstr.java

---
+++ parameterconstr.java
@@ -0,0 +1,22 @@
+class Student{
+	String name;
+	int rollNo;
+	String dept;
+	Student(String name,int rollNo,String dept){
+		this.name=name;
+		this.rollNo=rollNo;
+		this.dept=dept;
+	}
+	public void printStudentInfo(){
+		System.out.println("Name:\t"+name);
+		System.out.println("Roll NO:\t"+rollNo);
+		System.out.println("Department:\t"+dept);
+	}
+}
+public class Demo2 {
+	public static void main(String[] args){
+		Student std1=new Student("Noel",59,"CSE");
+		std1.printStudentInfo();
+		Student std2=new Student("Alwin",16,"CSE");
+	}
+}
